dfdaced6a23f19f446dd546c1bd3424fbcd15dde
AkbarAsghari <AkbarAsghari@outlook.com>
2022-07-09 09:37:20 +0430

    Make Split Between UI and Code

---
+++ IToolKit/Shared/NavMenu.razor.cs
@@ -0,0 +1,54 @@
+﻿using Bit.BlazorUI;
+
+namespace IToolKit.Shared
+{
+    partial class NavMenu
+    {
+        private void onChange(string value)
+        {
+            BasicNoToolTipNavLinks = AllNavLinks.Where(x => x.Name.Contains(value)).ToList();
+        }
+
+        private bool collapseNavMenu = true;
+
+        private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+
+        private List<BitNavLinkItem> BasicNoToolTipNavLinks = new List<BitNavLinkItem>();
+
+        private readonly List<BitNavLinkItem> AllNavLinks = new()
+        {
+            new BitNavLinkItem
+            {
+                Name = "Home",
+                Url = "http://example.com",
+                Title = "",
+                CollapseAriaLabel = "Collapse Home section",
+                ExpandAriaLabel = "Expand Home section",
+                IconName = BitIconName.Home,
+            },
+            new BitNavLinkItem
+            {
+                Name = "Converters",
+                IconName = BitIconName.ChangeEntitlements,
+                Links = new List<BitNavLinkItem>
+                {
+                    new BitNavLinkItem { Name = "Timestamp", Url = "http://msn.com", IconName= BitIconName.Clock },
+                }
+            },
+        };
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                BasicNoToolTipNavLinks = AllNavLinks;
+                await this.InvokeAsync(() => this.StateHasChanged());
+            }
+        }
+    }
+}
