4069f5d82714fc00ae43d11783d95dda5e4658a6
Akbar Asghari <akbarasghari@outlook.com>
2023-01-28 23:15:38 +0330

    Add Calculate MD5 to Generator Tools

--- IToolKit/Pages/Tools/Generators/Hash/Hash.razor.cs
+++ IToolKit/Pages/Tools/Generators/Hash/Hash.razor.cs
@@ -1,13 +1,18 @@
 ﻿using Bit.BlazorUI;
 using IToolKit.API.Enums.Tools.Hashings;
 using IToolKit.API.Tools.Hashings;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace IToolKit.Pages.Tools.Generators.Hash
 {
     public partial class Hash
     {
+        [Inject] IJSRuntime _JSRuntime { get; set; }
+
         string _CurrentValue;
 
+        string _MD5Result;
         string _SHA1Result;
         string _SHA256Result;
         string _SHA384Result;
@@ -18,20 +23,22 @@ namespace IToolKit.Pages.Tools.Generators.Hash
 
         private void OnChangeEvent(string value)
         {
+            Console.WriteLine(value);
             _CurrentValue = value;
 
             if (_IsAutoUpdate)
                 CalcHash();
         }
 
-        void CalcHash()
+        async void CalcHash()
         {
             if (String.IsNullOrWhiteSpace(_CurrentValue))
             {
-                _SHA1Result = _SHA256Result = _SHA384Result = _SHA512Result = String.Empty;
+                _MD5Result = _SHA1Result = _SHA256Result = _SHA384Result = _SHA512Result = String.Empty;
                 return;
             }
 
+            _MD5Result = await _JSRuntime.InvokeAsync<string>("md5", _CurrentValue);
             _SHA1Result = HashingsTool.ComputeSHA1Hash(_CurrentValue);
             _SHA256Result = HashingsTool.ComputeSHA256Hash(_CurrentValue);
             _SHA384Result = HashingsTool.ComputeSHA384Hash(_CurrentValue);
@@ -44,6 +51,7 @@ namespace IToolKit.Pages.Tools.Generators.Hash
         {
             if (!value)
             {
+                _MD5Result = _MD5Result.ToLower();
                 _SHA1Result = _SHA1Result.ToLower();
                 _SHA256Result = _SHA256Result.ToLower();
                 _SHA384Result = _SHA384Result.ToLower();
@@ -51,6 +59,7 @@ namespace IToolKit.Pages.Tools.Generators.Hash
             }
             else
             {
+                _MD5Result = _MD5Result.ToUpper();
                 _SHA1Result = _SHA1Result.ToUpper();
                 _SHA256Result = _SHA256Result.ToUpper();
                 _SHA384Result = _SHA384Result.ToUpper();
