6eecb1a4db7da165997dae22e52300c3b6ad9e2c
Akbar Asghari <akbarasghari@outlook.com>
2023-08-26 12:36:16 +0000

    Add Json Formatter

---
+++ IToolKit/Extensions/Extensions.cs
@@ -0,0 +1,10 @@
+namespace IToolKit.Extensions;
+public static class Extensions{
+    public static void ForEach<T>(this IEnumerable<T> ie, Action<T> action)
+    {
+        foreach (var i in ie)
+        {
+            action(i);
+        }
+    }
+}
---
+++ IToolKit/Pages/Tools/Formatters/Json/Json.razor.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace IToolKit.Pages.Tools.Formatters.Json;
+partial class Json
+{
+    string _InputValue;
+    string _JsonFormatedResult;
+
+    async Task Format()
+    {
+        if (String.IsNullOrEmpty(_InputValue))
+        {
+            _JsonFormatedResult = String.Empty;
+            return;
+        }
+
+        try
+        {
+          _JsonFormatedResult = JsonFormatterTools.FormatJson(_InputValue);
+        
+        }
+        catch (Exception)
+        {
+            _JsonFormatedResult = String.Empty;
+        }
+
+        await this.InvokeAsync(() => StateHasChanged());
+    }
+}
