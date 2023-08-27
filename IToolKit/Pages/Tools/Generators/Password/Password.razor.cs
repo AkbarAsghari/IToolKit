3feda03b4035afb5be0a08d8b3293e6d713e0af7
Akbar Asghari <akbarasghari@outlook.com>
2023-08-27 13:41:03 +0000

    Add Password Generator

---
+++ IToolKit/Pages/Tools/Generators/Password/Password.razor.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace IToolKit.Pages.Tools.Generators.Password
+{
+    public partial class Password
+    {
+        string _Result = String.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                _Result = CreatePassword(16);
+                _Result += $"\n{CreatePassword(16)}\n";
+                _Result += $"{CreatePassword(16)}\n";
+                _Result += $"{CreatePassword(16)}\n";
+                _Result += $"{CreatePassword(16)}\n";
+                _Result += $"{CreatePassword(16)}\n";
+                _Result += $"{CreatePassword(16)}\n";
+                _Result += $"{CreatePassword(16)}\n";
+                _Result += $"{CreatePassword(16)}\n";
+                _Result += $"{CreatePassword(16)}\n";
+                StateHasChanged();
+                await Task.Delay(100);
+            }
+        }
+
+        public string CreatePassword(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+    }
+}
