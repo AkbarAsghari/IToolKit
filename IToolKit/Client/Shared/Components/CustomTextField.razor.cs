c4206a13fb0404e220d66969f01be3704069622f
Akbar Asghari <akbarasghari@outlook.com>
2023-09-06 15:48:47 +0330

    Add CustomTextField

---
+++ IToolKit/Client/Shared/Components/CustomTextField.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+using MudBlazor;
+
+namespace IToolKit.Client.Shared.Components;
+partial class CustomTextField
+{
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        base.Variant = Variant.Outlined;
+    }
+
+    private async Task Copy()
+    {
+        await JSRuntime.InvokeVoidAsync("navigator.clipboard.writeText", Text);
+    }
+}
