eacacd423afa2fbd8e83f31a29b926316051b185
Akbar Asghari <akbarasghari@outlook.com>
2023-09-17 01:29:07 +0330

    add Versions

--- IToolKit/IToolKit/wwwroot/service-worker.published.js
+++ IToolKit/IToolKit/wwwroot/service-worker.published.js
@@ -1,6 +1,9 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
+//Versions
+const CACHE_VERSION = '0.16.9'
+
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
