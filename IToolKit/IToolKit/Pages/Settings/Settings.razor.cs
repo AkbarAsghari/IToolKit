34720e74032a20f94f4826a70ab6dc6d1c18fc9b
Akbar Asghari <akbarasghari@outlook.com>
2023-09-23 00:56:46 +0330

    Add Setting Page

---
+++ IToolKit/IToolKit/Pages/Settings/Settings.razor.cs
@@ -0,0 +1,15 @@
+﻿using IToolKit.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace IToolKit.Pages.Settings;
+
+partial class Settings
+{
+    [Inject] IJSRuntime _JSRuntime { get; set; }
+    async Task ClearCacheAndReDownload()
+    {
+        await _JSRuntime.ClearCache();
+        await _JSRuntime.Reload();
+    }
+}
