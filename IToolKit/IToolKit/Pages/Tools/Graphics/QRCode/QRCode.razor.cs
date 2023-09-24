1864d1e5c947ac5acaccb6e4c15846c1a8b2a753
Akbar Asghari <akbarasghari@outlook.com>
2023-09-24 14:11:47 +0330

    Add QR Code

---
+++ IToolKit/IToolKit/Pages/Tools/Graphics/QRCode/QRCode.razor.cs
@@ -0,0 +1,30 @@
+﻿using MudBlazor.Utilities;
+using QRCoder;
+
+namespace IToolKit.Pages.Tools.Graphics.QRCode;
+
+partial class QRCode
+{
+    QRCodeGenerator qrGenerator = new QRCodeGenerator();
+
+    string _Input;
+    string _Base64Image;
+    MudColor _LightColor = new MudColor("#fffff1ff");
+    MudColor _DarkColor = new MudColor("#000001ff");
+
+    async Task OnColorsChange()
+    {
+        await OnTextChange(_Input);
+    }
+
+    async Task OnTextChange(string value)
+    {
+        _Input = value;
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
+        PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+        _Base64Image = Convert.ToBase64String(qrCode.GetGraphic(pixelsPerModule: 20,
+            darkColorRgba: new byte[] { _DarkColor.R, _DarkColor.G, _DarkColor.B, _DarkColor.A },
+            lightColorRgba: new byte[] { _LightColor.R, _LightColor.G, _LightColor.B, _LightColor.A }));
+        await Task.CompletedTask;
+    }
+}
