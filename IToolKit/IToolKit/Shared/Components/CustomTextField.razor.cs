62767aac5fc85124da0db9735d0daab0320369ab
Akbar Asghari <akbarasghari@outlook.com>
2023-09-30 23:47:50 +0330

    Add QR Code Button for Results

--- IToolKit/IToolKit/Shared/Components/CustomTextField.razor.cs
+++ IToolKit/IToolKit/Shared/Components/CustomTextField.razor.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using IToolKit.API.Enums.Components;
+using IToolKit.API.Tools.Generators;
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using MudBlazor;
+using QRCoder;
+using System.Security.Cryptography.X509Certificates;
 
 namespace IToolKit.Shared.Components;
 partial class CustomTextField
 {
     [Parameter] public bool MonoSpace { get; set; }
+    [Inject] IDialogService DialogService { get; set; }
+
     protected override void OnInitialized()
     {
         base.OnInitialized();
@@ -26,5 +32,19 @@ partial class CustomTextField
     private async Task Copy()
     {
         await JSRuntime.InvokeVoidAsync("navigator.clipboard.writeText", Text);
+    }
+
+    private async Task GenerateQRCode()
+    {
+        await DialogService.ShowAsync<CustomImage>(
+             "QR Code",
+             parameters: new DialogParameters<CustomImage> {
+                { x => x.Src, QRCodeTools.TextToBase64(Text) },
+                { x => x.ImageType, ImageTypeEnum.Base64 } },
+             options: new DialogOptions
+             {
+                 MaxWidth = MaxWidth.ExtraSmall,
+                 CloseButton = true,
+             });
     }
 }
