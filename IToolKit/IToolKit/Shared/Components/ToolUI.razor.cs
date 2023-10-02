288ded2d00d9e271f2162a240767c94e30bbbf5f
Akbar Asghari <akbarasghari@outlook.com>
2023-10-02 22:09:19 +0330

    Add ToolUI Component

---
+++ IToolKit/IToolKit/Shared/Components/ToolUI.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace IToolKit.Shared.Components;
+partial class ToolUI
+{
+    [Parameter] public string Header { get; set; }
+    [Parameter] public RenderFragment ChildContent { get; set; }
+    [Parameter] public RenderFragment Actions { get; set; }
+}
